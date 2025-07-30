902c0a19c969c0da4140f6a39a4312aedab3b969
Call me Ali <aliyazdanpanahfard@gmail.com>
2025-07-30 12:23:58 +0330

    adding Links Routes

--- Shoping-Cart/src/components/navbar/Navbar.jsx
+++ Shoping-Cart/src/components/navbar/Navbar.jsx
@@ -1,15 +1,16 @@
 import "./Navbar.css";
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
   return (
     <>
       <div className="Navabar">
         <div className="NavSide">
-          <span>Product</span>
-          <span>About</span>
+          <span> <Link to={"/"} >Product</Link> </span>
+          <span> <Link to={"/about"}>About</Link> </span>
         </div>
         <div className="NavSide">
-          <span>Shoping Cart</span>
+          <span> <Link to={"/cart"}>Shoping Cart</Link> </span>
         </div>
       </div>
     </>
