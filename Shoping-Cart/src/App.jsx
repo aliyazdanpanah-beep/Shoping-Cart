ccb8269d936528285ba671bb4f4d429651f7cb4a
Call me Ali <aliyazdanpanahfard@gmail.com>
2025-07-31 13:47:12 +0330

    Update App.jsx

--- Shoping-Cart/src/App.jsx
+++ Shoping-Cart/src/App.jsx
@@ -1,20 +1,23 @@
 import "./App.css";
 import Home from "./page/Home/Home";
-import { useContext, createContext } from "react"
-import { Routes, Route } from "react-router-dom"
+import { useContext, createContext } from "react";
+import { Routes, Route } from "react-router-dom";
 import ProductPage from "./page/Product/Product";
 import About from "./page/About/about";
 import ShopingCart from "./page/ShpoingCart/ShopingCart";
+import { ShopContextProvider } from "./context/context";
 
 const App = () => {
   return (
     <div>
-      <Home />
-      <Routes>
-        <Route path="/" element={<ProductPage/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/Cart" element={<ShopingCart/>} />
-      </Routes>
+      <ShopContextProvider>
+        <Home />
+        <Routes>
+          <Route path="/" element={<ProductPage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/Cart" element={<ShopingCart />} />
+        </Routes>
+      </ShopContextProvider>
     </div>
   );
 };
