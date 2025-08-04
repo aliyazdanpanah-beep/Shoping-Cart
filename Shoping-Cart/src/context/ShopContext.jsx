42273f0e5dc974b13df0803b89280f9ff6a0095b
Call me Ali <aliyazdanpanahfard@gmail.com>
2025-08-04 17:00:50 +0330

    Update ShopContext.jsx

--- Shoping-Cart/src/context/ShopContext.jsx
+++ Shoping-Cart/src/context/ShopContext.jsx
@@ -4,10 +4,15 @@ export const ShopContext = createContext(null);
 
 export const ShopContextProvider = ({ children }) => {
   const [cartItem, setCartItem] = useState([]);
+  const [count, setCount] = useState(0);
 
-  const AddtoCart = () => {};
+  const AddtoCart = () => {
+    setCount(count + 1);
+  };
 
-  const RemoveFromCart = () => {};
+  const RemoveFromCart = () => {
+    setCount(count + 2);
+  };
 
   const ContextValue = { cartItem, AddtoCart, RemoveFromCart };
   return (
